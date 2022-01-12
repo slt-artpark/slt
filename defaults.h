7e1d38ac3f9dbc10ddb8b0435e30094f7c59d65d
glsukki <69789390+glsukki@users.noreply.github.com>
2022-01-12 14:45:41 +0530

    Added some constants

--- defaults.h
+++ defaults.h
@@ -6,4 +6,24 @@ const float DEFAULT_R_DIV = 10000.0;
 const float DEFAULT_FLAT_RESISTANCE = 482812.5;
 const float DEFAULT_BEND_RESISTANCE = 2507500.0;
 
+
+const float R_DIV_0 = 8730;
+const float R_DIV_1 = 8570;
+const float R_DIV_2 = 11850;
+const float R_DIV_3 = 8100;
+const float R_DIV_4 = 8890;
+
+
+const float FLAT_RESISTANCE_0 = 8730;
+const float FLAT_RESISTANCE_1 = 8570;
+const float FLAT_RESISTANCE_2 = 11850;
+const float FLAT_RESISTANCE_3 = 8100;
+const float FLAT_RESISTANCE_4 = 8890;
+
+const float BEND_RESISTANCE_0 = 12650;
+const float BEND_RESISTANCE_1 = 11500;
+const float BEND_RESISTANCE_2 = 18400;
+const float BEND_RESISTANCE_3 = 11400;
+const float BEND_RESISTANCE_4 = 12800;
+
 #endif
